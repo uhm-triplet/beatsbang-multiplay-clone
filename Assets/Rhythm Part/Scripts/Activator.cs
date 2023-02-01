5a8224b570ed1d5b2e7ac23403d80b96ecb86fc5
shane-sev <70693591+shane-sev@users.noreply.github.com>
2023-02-01 01:07:12 -1000

    Rhythm Tutorial 2 complete

--- Assets/Rhythm Part/Scripts/Activator.cs
+++ Assets/Rhythm Part/Scripts/Activator.cs
@@ -4,18 +4,29 @@ using UnityEngine;
 
 public class Activator : MonoBehaviour
 {
+    SpriteRenderer sr;
     public KeyCode key;
     bool active = false;
     GameObject note;
+    Color original;
+
+    void Awake() {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-
+        original = sr.color;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(key)) {
+            StartCoroutine(Clicked());
+        }
+
         if (Input.GetKeyDown(key) && active)
         {
             Destroy(note.gameObject);
@@ -36,5 +47,12 @@ public class Activator : MonoBehaviour
     void OnTriggerExit2D(Collider2D other)
     {
         active = false;
+    }
+
+    IEnumerator Clicked() {
+        Color original = sr.color;
+        sr.color = new Color(0, 0, 0);
+        yield return new WaitForSeconds(0.1f);
+        sr.color = original;
     }
 }
